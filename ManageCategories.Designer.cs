558c2f3a36877050cb26dd1402eb7c2450a67809
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2022-08-28 23:06:08 +0000

    designing the mange categories form

--- ManageCategories.Designer.cs
+++ ManageCategories.Designer.cs
@@ -29,77 +29,145 @@ namespace ManagementSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.button1 = new System.Windows.Forms.Button();
+            this.addcategory = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.updatecategory = new System.Windows.Forms.Button();
+            this.deletecategory = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.category_name = new System.Windows.Forms.TextBox();
+            this.category_des = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.category_id = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // splitter1
+            // addcategory
             // 
-            this.splitter1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(225, 450);
-            this.splitter1.TabIndex = 0;
-            this.splitter1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Black;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(631, 31);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Add a Category";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.addcategory.BackColor = System.Drawing.Color.Black;
+            this.addcategory.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addcategory.ForeColor = System.Drawing.Color.White;
+            this.addcategory.Location = new System.Drawing.Point(263, 198);
+            this.addcategory.Name = "addcategory";
+            this.addcategory.Size = new System.Drawing.Size(132, 32);
+            this.addcategory.TabIndex = 1;
+            this.addcategory.Text = "Add a Category";
+            this.addcategory.UseVisualStyleBackColor = false;
+            this.addcategory.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(265, 110);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(37, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.Size = new System.Drawing.Size(184, 25);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Total Categories";
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(279, 142);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(27, 19);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "0";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.label1.Text = "Manage Categories";
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(404, 164);
+            this.dataGridView1.Location = new System.Drawing.Point(132, 245);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(318, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(551, 193);
             this.dataGridView1.TabIndex = 4;
+            // 
+            // updatecategory
+            // 
+            this.updatecategory.BackColor = System.Drawing.Color.ForestGreen;
+            this.updatecategory.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.updatecategory.ForeColor = System.Drawing.Color.White;
+            this.updatecategory.Location = new System.Drawing.Point(411, 198);
+            this.updatecategory.Name = "updatecategory";
+            this.updatecategory.Size = new System.Drawing.Size(140, 32);
+            this.updatecategory.TabIndex = 5;
+            this.updatecategory.Text = "Update Category";
+            this.updatecategory.UseVisualStyleBackColor = false;
+            this.updatecategory.Click += new System.EventHandler(this.updatecategory_Click);
+            // 
+            // deletecategory
+            // 
+            this.deletecategory.BackColor = System.Drawing.Color.Red;
+            this.deletecategory.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.deletecategory.ForeColor = System.Drawing.Color.White;
+            this.deletecategory.Location = new System.Drawing.Point(570, 198);
+            this.deletecategory.Name = "deletecategory";
+            this.deletecategory.Size = new System.Drawing.Size(132, 32);
+            this.deletecategory.TabIndex = 6;
+            this.deletecategory.Text = "Delete Category";
+            this.deletecategory.UseVisualStyleBackColor = false;
+            this.deletecategory.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Category name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Category description";
+            // 
+            // category_name
+            // 
+            this.category_name.Location = new System.Drawing.Point(263, 77);
+            this.category_name.Name = "category_name";
+            this.category_name.Size = new System.Drawing.Size(439, 26);
+            this.category_name.TabIndex = 9;
+            // 
+            // category_des
+            // 
+            this.category_des.Location = new System.Drawing.Point(263, 122);
+            this.category_des.Name = "category_des";
+            this.category_des.Size = new System.Drawing.Size(439, 26);
+            this.category_des.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Category ID";
+            // 
+            // category_id
+            // 
+            this.category_id.Location = new System.Drawing.Point(263, 162);
+            this.category_id.Name = "category_id";
+            this.category_id.Size = new System.Drawing.Size(439, 26);
+            this.category_id.TabIndex = 12;
+            this.category_id.Tag = "";
             // 
             // ManageCategories
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.category_id);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.category_des);
+            this.Controls.Add(this.category_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.deletecategory);
+            this.Controls.Add(this.updatecategory);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.addcategory);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ManageCategories";
             this.Text = "ManageCategories";
@@ -110,11 +178,16 @@ namespace ManagementSystem
         }
 
         #endregion
-
-        private System.Windows.Forms.Splitter splitter1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addcategory;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button updatecategory;
+        private System.Windows.Forms.Button deletecategory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox category_name;
+        private System.Windows.Forms.TextBox category_des;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox category_id;
     }
 }
--- ManageCategories.cs
+++ ManageCategories.cs
@@ -7,21 +7,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ManagementSystem
 {
     public partial class ManageCategories : Form
     {
+        public static string connectionstring = "Datasource = 127.0.0.1; username=root;password=;database=supermarket;Allow User Variables=True";
+
+        MySqlConnection connection = new MySqlConnection();
         public ManageCategories()
         {
             InitializeComponent();
+            loaddata();
+        }
+        public void loaddata()
+        {
+            connection = new MySqlConnection(connectionstring);
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "Select * from categories";
+            MySqlDataAdapter da = new MySqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            AddCategory addCategory = new AddCategory();
-            addCategory.Show();
+            String name = category_name.Text;
+            String description = category_des.Text;
+            if (name != "" && description != "")
+
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "insert into categories(category_name,category_description) values (?name, ?description )";
+
+                command.Parameters.AddWithValue("?name", name);
+                command.Parameters.AddWithValue("?description", description);
+                command.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Category has been added succesfully");
+                loaddata();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+            //this.Hide();
+            //AddCategory addCategory = new AddCategory();
+            //addCategory.Show();
+        }
+
+        private void updatecategory_Click(object sender, EventArgs e)
+        {
+            String name = category_name.Text;
+            String description = category_des.Text;
+            String id = category_id.Text;
+            if (name != "" && description != "" && id!="")
+
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "update categories set category_name=?name,category_description=?description where category_id=?id";
+
+                command.Parameters.AddWithValue("?name", name);
+                command.Parameters.AddWithValue("?description", description);
+                command.Parameters.AddWithValue("?id", id);
+                command.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Category has been updated succesfully");
+                loaddata();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String name = category_name.Text;
+            String description = category_des.Text;
+            String id = category_id.Text;
+            if (name != "" && description != "" && id != "")
+
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "delete from categories where category_id=?id";
+
+                
+                command.Parameters.AddWithValue("?id", id);
+                command.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Category has been deleted succesfully");
+                loaddata();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
         }
     }
 }
