a9299c96509eb99edb4ebc09859f48105499e058
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2022-08-29 00:31:08 +0000

    designing the manage products form

--- ManageProducts.Designer.cs
+++ ManageProducts.Designer.cs
@@ -29,7 +29,6 @@ namespace ManagementSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitter1 = new System.Windows.Forms.Splitter();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,24 +37,31 @@ namespace ManagementSystem
             this.total_products = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.updateproduct = new System.Windows.Forms.Button();
+            this.deleteproduct = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.product_id = new System.Windows.Forms.TextBox();
+            this.product_name = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.product_des = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.product_code = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.product_price = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.reorder_level = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.product_quantity = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // splitter1
-            // 
-            this.splitter1.BackColor = System.Drawing.Color.Black;
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(244, 450);
-            this.splitter1.TabIndex = 0;
-            this.splitter1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(323, 38);
+            this.label1.Location = new System.Drawing.Point(32, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(164, 25);
             this.label1.TabIndex = 1;
@@ -66,7 +72,7 @@ namespace ManagementSystem
             this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(647, 38);
+            this.button1.Location = new System.Drawing.Point(193, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 29);
             this.button1.TabIndex = 2;
@@ -77,7 +83,7 @@ namespace ManagementSystem
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(268, 99);
+            this.label2.Location = new System.Drawing.Point(264, 14);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(111, 20);
             this.label2.TabIndex = 3;
@@ -86,7 +92,7 @@ namespace ManagementSystem
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(402, 99);
+            this.label3.Location = new System.Drawing.Point(398, 14);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(98, 20);
             this.label3.TabIndex = 4;
@@ -95,7 +101,7 @@ namespace ManagementSystem
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(530, 99);
+            this.label4.Location = new System.Drawing.Point(526, 14);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(88, 20);
             this.label4.TabIndex = 5;
@@ -106,7 +112,7 @@ namespace ManagementSystem
             this.total_products.AutoSize = true;
             this.total_products.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.total_products.ForeColor = System.Drawing.Color.RosyBrown;
-            this.total_products.Location = new System.Drawing.Point(267, 129);
+            this.total_products.Location = new System.Drawing.Point(263, 34);
             this.total_products.Name = "total_products";
             this.total_products.Size = new System.Drawing.Size(23, 25);
             this.total_products.TabIndex = 6;
@@ -118,7 +124,7 @@ namespace ManagementSystem
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label6.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label6.Location = new System.Drawing.Point(401, 129);
+            this.label6.Location = new System.Drawing.Point(397, 34);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(23, 25);
             this.label6.TabIndex = 7;
@@ -129,36 +135,184 @@ namespace ManagementSystem
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label7.ForeColor = System.Drawing.Color.RosyBrown;
-            this.label7.Location = new System.Drawing.Point(529, 129);
+            this.label7.Location = new System.Drawing.Point(525, 34);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(23, 25);
             this.label7.TabIndex = 8;
             this.label7.Text = "0";
             // 
-            // panel1
+            // updateproduct
             // 
-            this.panel1.Location = new System.Drawing.Point(268, 219);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(503, 191);
-            this.panel1.TabIndex = 9;
+            this.updateproduct.BackColor = System.Drawing.Color.ForestGreen;
+            this.updateproduct.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.updateproduct.ForeColor = System.Drawing.Color.White;
+            this.updateproduct.Location = new System.Drawing.Point(333, 227);
+            this.updateproduct.Name = "updateproduct";
+            this.updateproduct.Size = new System.Drawing.Size(144, 29);
+            this.updateproduct.TabIndex = 10;
+            this.updateproduct.Text = "Update Product";
+            this.updateproduct.UseVisualStyleBackColor = false;
+            this.updateproduct.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // deleteproduct
+            // 
+            this.deleteproduct.BackColor = System.Drawing.Color.Red;
+            this.deleteproduct.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.deleteproduct.ForeColor = System.Drawing.Color.White;
+            this.deleteproduct.Location = new System.Drawing.Point(495, 227);
+            this.deleteproduct.Name = "deleteproduct";
+            this.deleteproduct.Size = new System.Drawing.Size(138, 29);
+            this.deleteproduct.TabIndex = 11;
+            this.deleteproduct.Text = "Delete Product";
+            this.deleteproduct.UseVisualStyleBackColor = false;
+            this.deleteproduct.Click += new System.EventHandler(this.deleteproduct_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Product ID";
+            // 
+            // product_id
+            // 
+            this.product_id.Location = new System.Drawing.Point(162, 68);
+            this.product_id.Name = "product_id";
+            this.product_id.Size = new System.Drawing.Size(169, 26);
+            this.product_id.TabIndex = 13;
+            // 
+            // product_name
+            // 
+            this.product_name.Location = new System.Drawing.Point(162, 106);
+            this.product_name.Name = "product_name";
+            this.product_name.Size = new System.Drawing.Size(169, 26);
+            this.product_name.TabIndex = 15;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label8.Location = new System.Drawing.Point(268, 175);
+            this.label8.Location = new System.Drawing.Point(33, 150);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(232, 25);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Recently Added Products";
+            this.label8.Size = new System.Drawing.Size(98, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Product des.";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // product_des
+            // 
+            this.product_des.Location = new System.Drawing.Point(162, 150);
+            this.product_des.Name = "product_des";
+            this.product_des.Size = new System.Drawing.Size(169, 26);
+            this.product_des.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(33, 106);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 20);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Product name";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // product_code
+            // 
+            this.product_code.Location = new System.Drawing.Point(530, 68);
+            this.product_code.Name = "product_code";
+            this.product_code.Size = new System.Drawing.Size(169, 26);
+            this.product_code.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(401, 68);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(103, 20);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Product code";
+            // 
+            // product_price
+            // 
+            this.product_price.Location = new System.Drawing.Point(530, 106);
+            this.product_price.Name = "product_price";
+            this.product_price.Size = new System.Drawing.Size(169, 26);
+            this.product_price.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(401, 106);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 20);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Product price";
+            // 
+            // reorder_level
+            // 
+            this.reorder_level.Location = new System.Drawing.Point(530, 150);
+            this.reorder_level.Name = "reorder_level";
+            this.reorder_level.Size = new System.Drawing.Size(169, 26);
+            this.reorder_level.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(401, 150);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(102, 20);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Reorder level";
+            // 
+            // product_quantity
+            // 
+            this.product_quantity.Location = new System.Drawing.Point(162, 195);
+            this.product_quantity.Name = "product_quantity";
+            this.product_quantity.Size = new System.Drawing.Size(169, 26);
+            this.product_quantity.TabIndex = 25;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(33, 195);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 20);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "Product quantity";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 277);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 161);
+            this.dataGridView1.TabIndex = 26;
             // 
             // ManageProducts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.product_quantity);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.reorder_level);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.product_price);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.product_code);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.product_des);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.product_name);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.product_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.deleteproduct);
+            this.Controls.Add(this.updateproduct);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.total_products);
@@ -167,18 +321,16 @@ namespace ManagementSystem
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.splitter1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ManageProducts";
             this.Text = "ManageProducts";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
@@ -187,7 +339,22 @@ namespace ManagementSystem
         private System.Windows.Forms.Label total_products;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button updateproduct;
+        private System.Windows.Forms.Button deleteproduct;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox product_id;
+        private System.Windows.Forms.TextBox product_name;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox product_des;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox product_code;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox product_price;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox reorder_level;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox product_quantity;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- ManageProducts.cs
+++ ManageProducts.cs
@@ -19,13 +19,54 @@ namespace ManagementSystem
         public ManageProducts()
         {
             InitializeComponent();
+            loaddata();
+        }
+        public void loaddata()
+        {
+            connection = new MySqlConnection(connectionstring);
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "Select * from Products";
+            MySqlDataAdapter da = new MySqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.SendToBack();
-            AddProduct addProduct = new AddProduct();
-            addProduct.Show();
+            String name = product_name.Text;
+            String price = product_price.Text;
+            String des = product_des.Text;
+            String reorder = reorder_level.Text;
+            String quantity = product_quantity.Text;
+            String code = product_code.Text;
+            if (name != "" && price != "" && des!=""&& reorder!=""&&quantity!=""&&code!="")
+
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "insert into products(product_name,product_price,product_description,product_quantity,product_qrcode,reorder_level) values (?name, ?price,?des,?quantity,?code,?reorder )";
+
+                command.Parameters.AddWithValue("?name", name);
+                command.Parameters.AddWithValue("?price", price);
+                command.Parameters.AddWithValue("?des", des);
+                command.Parameters.AddWithValue("?quantity", quantity);
+                command.Parameters.AddWithValue("?code", code);
+                command.Parameters.AddWithValue("?reorder", reorder);
+                command.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Product has been added succesfully");
+                loaddata();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
 
         }
 
@@ -48,6 +89,94 @@ namespace ManagementSystem
                 // Else make id = "" so you can later check it.
                 totalproducts = 0;
             //total_products.Text.Replace("0", totalproducts);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String name = product_name.Text;
+            String price = product_price.Text;
+            String des = product_des.Text;
+            String reorder = reorder_level.Text;
+            String quantity = product_quantity.Text;
+            String code = product_code.Text;
+            String id = product_id.Text;
+            if (name != "" && price != "" && des != "" && reorder != "" && quantity != "" && code != "" && id!="")
+
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "update products set product_name=?name,product_price=?price,product_description=?des,product_quantity=?quantity,product_qrcode=?code,reorder_level=?reorder where product_id=?id";
+
+                command.Parameters.AddWithValue("?name", name);
+                command.Parameters.AddWithValue("?price", price);
+                command.Parameters.AddWithValue("?des", des);
+                command.Parameters.AddWithValue("?quantity", quantity);
+                command.Parameters.AddWithValue("?code", code);
+                command.Parameters.AddWithValue("?reorder", reorder);
+                command.Parameters.AddWithValue("?id", id);
+                command.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Product has been updated succesfully");
+                loaddata();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteproduct_Click(object sender, EventArgs e)
+        {
+            String name = product_name.Text;
+            String price = product_price.Text;
+            String des = product_des.Text;
+            String reorder = reorder_level.Text;
+            String quantity = product_quantity.Text;
+            String code = product_code.Text;
+            String id = product_id.Text;
+            if (name != "" && price != "" && des != "" && reorder != "" && quantity != "" && code != "" && id != "")
+
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "delete from products where product_id=?id";
+
+                command.Parameters.AddWithValue("?name", name);
+                command.Parameters.AddWithValue("?price", price);
+                command.Parameters.AddWithValue("?des", des);
+                command.Parameters.AddWithValue("?quantity", quantity);
+                command.Parameters.AddWithValue("?code", code);
+                command.Parameters.AddWithValue("?reorder", reorder);
+                command.Parameters.AddWithValue("?id", id);
+                command.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show("Product has been deleted succesfully");
+                loaddata();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
 
         }
     }
